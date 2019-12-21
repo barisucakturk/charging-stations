e5aad8ed736db396c3f66bffbcfdcd59f3834052
Baris Ucakturk <baris.ucakturk@tokigames.com>
2019-12-21 13:54:11 +0300

    initial commit

---
+++ src/test/java/com/project/ucakturk/charging/stattion/chargingstation/ChargingStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ucakturk.charging.stattion.chargingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
